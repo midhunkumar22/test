dbc0bb45725c8309be2dafee6f848e93676b0080
MK <midhunkumar04@gmail.com>
2021-07-02 11:36:49 +0530

    Staged

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
-@app.route("/", methods =["GET"])
+@app.route("/", methods =["GET","POST"])
 def home():
-    return "Hello, World!"
-@app.route("/", methods =["POST"])   
-def End():
-    return "Bye, World!"
+  if request.method == "GET":
+      return "This is GET Request"
+  elif request.method == "POST":
+       return "This is POST Method"
+  else :
+      return "this is " + request.method + "method"
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
